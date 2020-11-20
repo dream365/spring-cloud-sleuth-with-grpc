c2507bb10fb12d7e3e512ed476c1a1fe35cdcb82
dream365 <kosewon_@naver.com>
2020-11-20 13:12:15 +0900

    Add service c

---
+++ service-c/src/main/kotlin/com/dream365/example/grpc/GrpcTestService.kt
@@ -0,0 +1,20 @@
+package com.dream365.example.grpc
+
+import com.dream365.example.ReactorTestServiceGrpc
+import com.dream365.example.GetResultRequest
+import com.dream365.example.GetResultResponse
+import org.lognet.springboot.grpc.GRpcService
+import reactor.core.publisher.Mono
+
+@GRpcService
+class GrpcTestService : ReactorTestServiceGrpc.TestServiceImplBase() {
+    override fun getResult(request: Mono<GetResultRequest>): Mono<GetResultResponse> = request
+        .map {
+            buildResponse("${it.msg}->C")
+        }
+
+    private fun buildResponse(responseMsg: String) = GetResultResponse
+        .newBuilder()
+        .setRes(responseMsg)
+        .build()
+}
